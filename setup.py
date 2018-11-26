e5a166ab8e128c0e50c41f6eb97d3e36ae490bfb
phroiland <jon@crowdkeep.com>
2018-11-26 16:26:33 -0500

    package upgrades

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-test-utils',
+    name='django-test-utils3',
     version='0.11.18',
     packages=find_packages(),
     author='Jon Froiland',
     author_email='jon@crowdkeep.com',
     description='A package to help testing in Django',
     url='https://github.com/phroiland/django-test-utils',
-    download_url='git@github.com:phroiland/django-test-utils.git',
+    download_url='https://github.com/phroiland/django-test-utils',
     test_suite='test_project.run_tests.run_tests',
     include_package_data=True,
     install_requires=[
