168ad3b976d7e31ec89bf62cd9ae7cd9d130b1a7
phroiland <jon@crowdkeep.com>
2018-11-27 15:03:28 -0500

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-test-utils3',
-    version='0.11.24',
+    version='0.11.25',
     packages=find_packages(),
     author='Jon Froiland',
     author_email='jon@crowdkeep.com',
