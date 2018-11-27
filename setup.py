829fd39e9112121e4e7b12f6786c0f47a813af3e
phroiland <jon@crowdkeep.com>
2018-11-26 21:23:59 -0500

    app

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-test-utils3',
-    version='0.11.14',
+    version='0.11.15',
     packages=find_packages(),
     author='Jon Froiland',
     author_email='jon@crowdkeep.com',
--- test_utils/testmaker/__init__.py
+++ test_utils/testmaker/__init__.py
@@ -49,6 +49,7 @@ class Testmaker(object):
 
     def set_paths(self):
         if self.app:
+            print('app:', self.app)
             self.app_name = self.app.__name__.split('.')[-2]
             self.base_dir = path.dirname(self.app.__file__)
         else:
