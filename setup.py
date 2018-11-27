08ce5fe7427c4ba634db8e779d2024457c4ce1ee
phroiland <jon@crowdkeep.com>
2018-11-26 20:09:07 -0500

    setup update

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     author='Jon Froiland',
     author_email='jon@crowdkeep.com',
     description='A package to help testing in Django',
+    long_description='Using 2to3, refactored code for Python3. Version references .<month>.<year>',
+    long_description_content_type='text/markdown',
     url='https://github.com/phroiland/django-test-utils',
     download_url='https://github.com/phroiland/django-test-utils',
     test_suite='test_project.run_tests.run_tests',
