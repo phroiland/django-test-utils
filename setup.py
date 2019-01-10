fedccb4597303b4d6a0b2e0686f11da248dce15d
phroiland <jon@crowdkeep.com>
2019-01-10 10:02:19 -0500

    settings.py and manage.py update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-test-utils3',
-    version='0.11.25',
+    version='1.10.1',
     packages=find_packages(),
     author='Jon Froiland',
     author_email='jon@crowdkeep.com',
